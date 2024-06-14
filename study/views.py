d2146556073a04ff7f094eba0323d85d0577c9ab
Selena Siri <selenasiri53@gmail.com>
2024-06-13 21:44:58 -0400

    index and room templates

--- study/views.py
+++ study/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': "Let's learn python!"},
+    {'id': 2, 'name': "Design with me"},
+    {'id': 3, 'name': "Frontend development"},
+]
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'base/index.html', {'rooms': rooms})
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
 
 # def index(request):
 #     return HttpResponse('Home Page')
