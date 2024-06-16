e83b90c4813e46621de116e21f1a4d553464fc49
Selena Siri <selenasiri53@gmail.com>
2024-06-16 16:28:12 -0400

    request rooms from database

--- study/models.py
+++ study/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    # host
+    # topic
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants = // all users active in a room
+    updated = models.DateTimeField(auto_now=True)
+        #timestamps everytime the save method is called
+    create = models.DateTimeField(auto_now_add=True)
+    #auto_now_add - only timestamps when we 1st save this instance, not each update
+
+    def __str__(self):
+        return str(self.name)
--- study/views.py
+++ study/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Room
 # from django.http import HttpResponse -- original
 
-rooms = [
-    {'id': 1, 'name': "Let's learn python!"},
-    {'id': 2, 'name': "Design with me"},
-    {'id': 3, 'name': "Frontend development"}
-]
+# rooms = [
+#     {'id': 1, 'name': "Let's learn python!"},
+#     {'id': 2, 'name': "Design with me"},
+#     {'id': 3, 'name': "Frontend development"}
+# ]
 
 def index(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/index.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'base/room.html', context)
